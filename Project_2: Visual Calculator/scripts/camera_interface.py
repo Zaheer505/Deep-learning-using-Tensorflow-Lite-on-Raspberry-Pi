8b780dffb9929acd8606fe55b7b98e0506581ba0
noshluk2 <noshluk2@gmail.com>
2022-07-07 21:14:56 +0500

    strter Project 2

---
+++ Project_2: Visual Calculator/scripts/camera_interface.py
@@ -0,0 +1,14 @@
+import cv2
+
+
+def main():
+    video_feed = cv2.VideoCapture()
+
+    while(1):
+        _,frame = video_feed.read()
+        cv2.imshow("camera_feed", frame)
+        cv2.waitKey(1)
+
+
+if __name__ == '__main__':
+    main()
